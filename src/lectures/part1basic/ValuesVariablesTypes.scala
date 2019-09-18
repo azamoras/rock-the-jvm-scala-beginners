0edf55be4b8231e9ae8826c20eb5a0dc003295ae
Adrian Zamora <zamora.suazo@gmail.com>
2019-09-17 22:51:16 -0300

    Comments. Part 1 basics.

--- src/lectures/part1basic/ValuesVariablesTypes.scala
+++ src/lectures/part1basic/ValuesVariablesTypes.scala
@@ -5,11 +5,12 @@ object ValuesVariablesTypes  extends App {
   val asInt: Int = 42;
   val asDouble: Double = 2.0
   val asFloat = 2.0f
-
+ // Val are Immutable
   println(asFloat)
 
   var asString: String = " hello, scala"
 
+  //Var are Mutable
   asString  += ", one more thing"
 
   println(asString)
