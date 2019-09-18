19766953d82d7dfe3c0a02e882e99ceb4bd8c67f
Adrian Zamora <zamora.suazo@gmail.com>
2019-09-17 22:49:28 -0300

    Part 1 Basic, Values, Variables and Types.  Hello World Playground.

---
+++ src/lectures/part1basic/ValuesVariablesTypes.scala
@@ -0,0 +1,17 @@
+package lectures.part1basic
+
+object ValuesVariablesTypes  extends App {
+
+  val asInt: Int = 42;
+  val asDouble: Double = 2.0
+  val asFloat = 2.0f
+
+  println(asFloat)
+
+  var asString: String = " hello, scala"
+
+  asString  += ", one more thing"
+
+  println(asString)
+
+}
